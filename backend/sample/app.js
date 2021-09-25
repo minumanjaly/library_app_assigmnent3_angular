afcc66154858c49e2f5409beb33b562a7890966e
minu <minumanjaly95@gmail.com>
2021-09-25 08:01:32 -0700

    first commit

---
+++ backend/sample/app.js
@@ -0,0 +1,3 @@
+var messenger = require("./messenger");
+messenger.printer("minu");
+
---
+++ backend/sample/messenger.js
@@ -0,0 +1,6 @@
+function greeter(name){
+    console.log(`Hello..!${name}`);
+
+}
+
+module.exports.printer = greeter;
